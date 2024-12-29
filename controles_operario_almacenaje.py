d1f858e07654706133ca492563ae5668a7746b89
santifco <santi.scalabrini@gmail.com>
2024-12-29 11:57:14 -0300

    Descripción de los cambios

--- controles_operario_almacenaje.py
+++ controles_operario_almacenaje.py
@@ -114,7 +114,7 @@ def mostrar_carta(data_row,posicion):
                 st.session_state.escaneada_posicion = ""  # Reinicia el campo de texto
                 # Incrementa la fila actual
                 st.session_state.current_row += 1
-                # st.rerun
+                st.rerun
 
     else: 
         st.warning("La posición ingresada es incorrecta.")
