632325a1ba81d18c7bebcb76d88ca2da8cf4390d
santifco <santi.scalabrini@gmail.com>
2025-01-03 13:02:38 -0300

    Descripción de los cambios

--- controles.py
+++ controles.py
@@ -251,7 +251,7 @@ if "Control Almacenaje" in seleccion:
             df_concatenado["Posicion"] = df_concatenado["Posicion"].str.rstrip()
             df_concatenado['Ordenar_primero'] = df_concatenado['Posicion'].str.split(' - ').str[0].str[2:4]
             df_concatenado['Ordenar_segundo'] = df_concatenado['Posicion'].str.split(' - ').str[1].astype(int)
-            df_concatenado = df_concatenado.sort_values(by=['Ordenar_primero', 'Ordenar_segundo']).drop(columns=['Ordenar_primero', 'Ordenar_segundo'])
+            df_concatenado = df_concatenado.sort_values(by=['Ordenar_primero', 'Ordenar_segundo'])
             # Tamaño de la población
             N_almacenaje = len(df_concatenado)
             st.write(f"Tamaño df_stock (número de filas en df_stock): {N_almacenaje}")
